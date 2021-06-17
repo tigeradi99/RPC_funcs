d575041434566e8084f662911e6b4638ea7eddad
tigeradi99 <adityanair483@gmail.com>
2021-06-18 00:23:59 +0530

    Combined sending and recieving functionality in interface.py

--- interface/interface.py
+++ interface/interface.py
@@ -1,35 +1,73 @@
 import messagefile_pb2 as message_out
 import time 
+import queue
 class I2C_Sensor:
 
-    def __init__(self, client_handle = None, topic = None):
-        self.foo = "bar"
+    def __init__(self, client_handle, topic):
+        #self.foo = "bar"
         #Initialize the following: They should be passed as a parameter when an object of this class is created
         # the MQTT client handle
         #Also initialize a data buffer tx_data to store data which will be sent over MQTT
+        self.client_handle = client_handle
+        self.topic = topic
+        self.client_handle.on_message = self.on_message
+        self.client_handle.subscribe("+/xRPC_Response")
+        self.data_outbox = message_out.xRPCMessage()    #Store incoming message
+        self.data_inbox = message_out.xRPCMessage()     #Store message to be serialized
+        self.tx_data = None                             #Store serialized message to be published
+        self.message_q = queue.Queue()
+        self.temp = float(0)
+        self.humidity = float(0)
+        self.pressure = float(0)
+
         
-    def serialize_data(self, sda_pin , scl_pin):
+    def serialize_data(self, sda_pin = 21 , scl_pin = 22):
         #First fill in the fields for timestamp
-        #data_outbox = message_in.xRPCMessage() -> This is the buffer created
-        
         #COnsult the Google documentation for Protocol Buffers in Python
 
-        #sec1 , usec1 = divmod(time.time(),1)
-        #time_stamp = foo.TimeVal()
-        #time_stamp.tv_sec = int(sec1)
-        #time_stamp.tv_usec = int(usec1 * 1000000)
+        sec1 , usec1 = divmod(time.time(),1)
+        time_stamp = message_out.TimeVal()
+        time_stamp.tv_sec = int(sec1)
+        time_stamp.tv_usec = int(usec1 * 1000000)
+        self.data_outbox.time_stamp.CopyFrom(time_stamp)
 
         #Serialize the data with given parameters (Only serialize data for I2C sensor read request, ignore the rest)
         #foo.request.i2c_request.slave_sda_gpio = int(sda)
         #foo_outbox.request.i2c_request.slave_scl_gpio = int(scl)
 
+        self.data_outbox.request.i2c_request.slave_scl_gpio = int(scl_pin)
+        self.data_outbox.request.i2c_request.slave_sda_gpio = int(sda_pin)
         #Consult the messagefile.proto definition for field names
-        
-        foo = "bar"
 
-        #self.tx_data = foo_outbox.SerializeToString() -> serializing buffer to string, which will be sent via MQTT
+        self.tx_data = self.data_outbox.SerializeToString() #serializing buffer to string, which will be sent via MQTT
+        print("Done serializing.")
+    
+    def on_message(self, client, userdata, message):
+        self.message_q.put(message)
+        client.loop_stop()
+
+    def deserialize_message(self):
+        if(not self.message_q.empty()):
+            print("Empty Queue: No message recieved yet")
+
+        m = self.message_q.get() #Extract recieved message from queue
+            
+        inb = m.payload
+
+        self.data_inbox.ParseFromString(inb)
+
+        if self.data_inbox.response.HasField("i2c_response"):
+            if self.data_inbox.response.i2c_response.success == 0:
+                self.temp = self.data_inbox.response.i2c_response.temp
+                self.humidity = self.data_inbox.response.i2c_response.rel_hum
+                self.pressure = self.data_inbox.response.i2c_response.pres
         
+        return self.temp, self.humidity, self.pressure
     
     def send(self):
         #Send the serialized data
-        foo = "bar"
+        print("Subscribed")
+        self.client_handle.loop_start()
+        self.client_handle.publish(self.topic, self.tx_data, qos = 0)
+        print("Data sent")
+        
---
+++ interface/test.py
@@ -0,0 +1,21 @@
+from interface2 import I2C_Sensor
+from reciever import Reciever
+import datetime
+import paho.mqtt.client as paho
+
+if __name__=="__main__":
+    broker = "test.mosquitto.org"
+    port = 1883
+    ts = datetime.datetime.now().isoformat()
+    c = 'client-' + ts[-6:]
+    client= paho.Client(c)
+    client.connect(broker,port)
+    sensor = I2C_Sensor(client, "101/xRPC_Request") # Initialize the sensor class
+    sensor.serialize_data(21,22) #serialize read request
+    sensor.send() #send request via MQTT, wait for a response and store it in a queue
+    
+    #Extract message from queue and return parameters
+    #temp, hum, pres = sensor.deserialize_message() 
+    
+    #print(f'Temperature: {temp}, Pressure: {pres}, Humidity: {hum}') 
+    client.disconnect()
